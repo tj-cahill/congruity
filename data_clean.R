175917378b34cd7a99b166cdc55315efd591e039
Tiernan Cahill <tjcahill@bu.edu>
2020-05-07 19:31:29 -0400

    Create data_clean.R

---
+++ data_clean.R
@@ -0,0 +1,8 @@
+######################################################################
+# DATA_CLEAN.R                                                       #
+# ================================================================== #
+# This script takes input from Qualtrics CSV export (for pre- and    #
+# post-test questionnaires) and from the Congruity stimulus software #
+# itself (for sequencing) and cleans it in a reproducible way,       #
+# ouputting the study data in a format suitable for analysis.        #
+######################################################################
